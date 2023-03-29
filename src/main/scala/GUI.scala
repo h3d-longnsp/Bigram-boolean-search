6f610e571535ebbc855c64186946448fedf4a4b2
h3d-longnsp <hydroxyzine@metacity.email>
2023-03-29 18:45:45 +0700

    Reformat code

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,4 +1,12 @@
-import javax.swing.{ JFrame, WindowConstants, JMenu, JMenuBar, JMenuItem, JSeparator, JFileChooser }
+import javax.swing.{
+  JFrame,
+  WindowConstants,
+  JMenu,
+  JMenuBar,
+  JMenuItem,
+  JSeparator,
+  JFileChooser
+}
 import java.awt.event.ActionListener
 import java.awt.event.ActionEvent
 import javax.swing.filechooser.FileNameExtensionFilter
@@ -12,142 +20,151 @@ import javax.swing.ScrollPaneConstants
 import javax.swing.UIManager
 
 object GUI {
-    var index: Map[String, List[Int]] = Map.empty
-    var inputFolderPath = ""
-    var indexFilePath = ""
-    val vocabTextArea = new JTextArea()
-    val filePathTextArea = new JTextArea(1, 1)
-    val folderPathTextArea = new JTextArea(1, 1)
+  var index: Map[String, List[Int]] = Map.empty
+  var inputFolderPath = ""
+  var indexFilePath = ""
+  val vocabTextArea = new JTextArea()
+  val filePathTextArea = new JTextArea(1, 1)
+  val folderPathTextArea = new JTextArea(1, 1)
 
-    private def createWindow(): Unit = {
-        val frame = new JFrame("Bigram Boolean Search")
-        val panel = new JPanel();
-        panel.setLayout(null)
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10))
-        frame.setSize(1050, 750)
-        frame.setResizable(false)
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-        createUI(frame, panel)
-        // frame.pack()
-        frame.add(panel)
-        frame.setVisible(true)
-    }
+  private def createWindow(): Unit = {
+    val frame = new JFrame("Bigram Boolean Search")
+    val panel = new JPanel();
+    panel.setLayout(null)
+    panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10))
+    frame.setSize(1050, 750)
+    frame.setResizable(false)
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    createUI(frame, panel)
+    // frame.pack()
+    frame.add(panel)
+    frame.setVisible(true)
+  }
 
-    private def createUI(frame: JFrame, panel: JPanel): Unit = {
-        createMenuBar(frame)
-        createPathsArea(panel)
-        createVocabArea(panel)
-        createButton(panel)
-    }
+  private def createUI(frame: JFrame, panel: JPanel): Unit = {
+    createMenuBar(frame)
+    createPathsArea(panel)
+    createVocabArea(panel)
+    createButton(panel)
+  }
 
-    def createMenuBar(frame: JFrame): Unit = {
-        val fileMenu = new JMenu("File")
-        val helpMenu = new JMenu("Help")
-        
-        val fileChooser = new JFileChooser()
-        val filter = new FileNameExtensionFilter("Index text file (*.txt)", "txt");
-        fileChooser.setFileFilter(filter)
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY)
-        fileChooser.setDialogTitle("Choose an index file")
+  def createMenuBar(frame: JFrame): Unit = {
+    val fileMenu = new JMenu("File")
+    val helpMenu = new JMenu("Help")
 
-        val folderChooser = new JFileChooser()
-        folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        folderChooser.setDialogTitle("Choose an input folder")
+    val fileChooser = new JFileChooser()
+    val filter = new FileNameExtensionFilter("Index text file (*.txt)", "txt");
+    fileChooser.setFileFilter(filter)
+    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY)
+    fileChooser.setDialogTitle("Choose an index file")
 
-        val fileMenuItem1 = new JMenuItem("Open input folder")
-        val fileMenuItem2 = new JMenuItem("Open index")
-        val fileMenuItem3 = new JMenuItem("Quit")
+    val folderChooser = new JFileChooser()
+    folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    folderChooser.setDialogTitle("Choose an input folder")
 
-        fileMenuItem1.addActionListener(new ActionListener {
-          override def actionPerformed(e: ActionEvent): Unit = {
-            if (folderChooser.showOpenDialog(fileMenuItem1) == JFileChooser.APPROVE_OPTION) {
-                inputFolderPath = folderChooser.getSelectedFile().getPath()
-                folderPathTextArea.setText(inputFolderPath)
-            }
-          }
-        })
+    val fileMenuItem1 = new JMenuItem("Open input folder")
+    val fileMenuItem2 = new JMenuItem("Open index")
+    val fileMenuItem3 = new JMenuItem("Quit")
 
-        fileMenuItem2.addActionListener(new ActionListener {
-          override def actionPerformed(e: ActionEvent): Unit = {
-            if (fileChooser.showOpenDialog(fileMenuItem1) == JFileChooser.APPROVE_OPTION) {
-                indexFilePath = fileChooser.getSelectedFile().getPath()
-                filePathTextArea.setText(indexFilePath)
-            }
-          }
-        })
+    fileMenuItem1.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        if (
+          folderChooser
+            .showOpenDialog(fileMenuItem1) == JFileChooser.APPROVE_OPTION
+        ) {
+          inputFolderPath = folderChooser.getSelectedFile().getPath()
+          folderPathTextArea.setText(inputFolderPath)
+        }
+      }
+    })
 
-        fileMenuItem3.addActionListener(new ActionListener {
-          override def actionPerformed(e: ActionEvent): Unit = {
-            System.exit(0)
-          }
-        })
+    fileMenuItem2.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        if (
+          fileChooser
+            .showOpenDialog(fileMenuItem1) == JFileChooser.APPROVE_OPTION
+        ) {
+          indexFilePath = fileChooser.getSelectedFile().getPath()
+          filePathTextArea.setText(indexFilePath)
+        }
+      }
+    })
 
-        fileMenu.add(fileMenuItem1)
-        fileMenu.add(fileMenuItem2)
-        fileMenu.add(new JSeparator()); // SEPARATOR        
-        fileMenu.add(fileMenuItem3)
+    fileMenuItem3.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        System.exit(0)
+      }
+    })
 
-        val menuBar = new JMenuBar
-        menuBar.add(fileMenu)
-        menuBar.add(helpMenu)
-        frame.setJMenuBar(menuBar)
-    }
+    fileMenu.add(fileMenuItem1)
+    fileMenu.add(fileMenuItem2)
+    fileMenu.add(new JSeparator()); // SEPARATOR
+    fileMenu.add(fileMenuItem3)
 
-    def createButton(panel: JPanel): Unit = {
-        val buildVocabBtn = new JButton("Build Vocab")
-        buildVocabBtn.setBounds(30, 650, 150, 25)
+    val menuBar = new JMenuBar
+    menuBar.add(fileMenu)
+    menuBar.add(helpMenu)
+    frame.setJMenuBar(menuBar)
+  }
 
-        val buildIndexBtn = new JButton("Build Index")
-        buildIndexBtn.setBounds(200, 650, 150, 25)
+  def createButton(panel: JPanel): Unit = {
+    val buildVocabBtn = new JButton("Build Vocab")
+    buildVocabBtn.setBounds(30, 650, 150, 25)
 
-        buildVocabBtn.addActionListener(new ActionListener {
-          override def actionPerformed(e: ActionEvent): Unit = {
-            val liststr = List("123", "asafa")
-            vocabTextArea.setText(liststr.mkString("\n"))
-          }
-        })
+    val buildIndexBtn = new JButton("Build Index")
+    buildIndexBtn.setBounds(200, 650, 150, 25)
 
-        panel.add(buildVocabBtn)
-        panel.add(buildIndexBtn)
-    }
+    buildVocabBtn.addActionListener(new ActionListener {
+      override def actionPerformed(e: ActionEvent): Unit = {
+        val liststr = List("123", "asafa")
+        vocabTextArea.setText(liststr.mkString("\n"))
+      }
+    })
 
-    def createVocabArea(panel: JPanel): Unit = {
-        vocabTextArea.setEditable(false)
-        vocabTextArea.setColumns(1)
-        
-        val borderVocab = BorderFactory.createTitledBorder("Vocabulary")
-        vocabTextArea.setBorder(borderVocab)
+    panel.add(buildVocabBtn)
+    panel.add(buildIndexBtn)
+  }
 
-        val scrollPane = new JScrollPane(vocabTextArea)
-        scrollPane.setBounds(30, 125, 150, 515)
-        panel.add(scrollPane)
-    }
+  def createVocabArea(panel: JPanel): Unit = {
+    vocabTextArea.setEditable(false)
+    vocabTextArea.setColumns(1)
 
-    def createPathsArea(panel: JPanel): Unit = {
-        UIManager.put("ScrollBar.width", 10);
-        
-        folderPathTextArea.setEditable(false)
-        filePathTextArea.setEditable(false)
-        
-        val border1 = BorderFactory.createTitledBorder("Input folder")
-        val scrollPane1 = new JScrollPane(folderPathTextArea)
-        scrollPane1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)
-        folderPathTextArea.setBorder(border1)
+    val borderVocab = BorderFactory.createTitledBorder("Vocabulary")
+    vocabTextArea.setBorder(borderVocab)
 
-        val border2 = BorderFactory.createTitledBorder("Index file")
-        val scrollPane2 = new JScrollPane(filePathTextArea)
-        scrollPane2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)        
-        filePathTextArea.setBorder(border2)
+    val scrollPane = new JScrollPane(vocabTextArea)
+    scrollPane.setBounds(30, 125, 150, 515)
+    panel.add(scrollPane)
+  }
 
-        scrollPane1.setBounds(30, 5, 325, 45)
-        scrollPane2.setBounds(30, 60, 325, 45)
+  def createPathsArea(panel: JPanel): Unit = {
+    UIManager.put("ScrollBar.width", 10);
 
-        panel.add(scrollPane1)
-        panel.add(scrollPane2)        
-    }
+    folderPathTextArea.setEditable(false)
+    filePathTextArea.setEditable(false)
 
-    def main(args: Array[String]) {
-        createWindow()
-    }
+    val border1 = BorderFactory.createTitledBorder("Input folder")
+    val scrollPane1 = new JScrollPane(folderPathTextArea)
+    scrollPane1.setVerticalScrollBarPolicy(
+      ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER
+    )
+    folderPathTextArea.setBorder(border1)
+
+    val border2 = BorderFactory.createTitledBorder("Index file")
+    val scrollPane2 = new JScrollPane(filePathTextArea)
+    scrollPane2.setVerticalScrollBarPolicy(
+      ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER
+    )
+    filePathTextArea.setBorder(border2)
+
+    scrollPane1.setBounds(30, 5, 325, 45)
+    scrollPane2.setBounds(30, 60, 325, 45)
+
+    panel.add(scrollPane1)
+    panel.add(scrollPane2)
+  }
+
+  def main(args: Array[String]) {
+    createWindow()
+  }
 }
-
