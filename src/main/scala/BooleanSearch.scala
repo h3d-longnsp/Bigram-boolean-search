cb805ecdb909a071dd24a263ce24a2b4403824d6
h3d-longnsp <hydroxyzine@metacity.email>
2023-03-31 23:03:24 +0700

    Add documentations

--- src/main/scala/BooleanSearch.scala
+++ src/main/scala/BooleanSearch.scala
@@ -1,22 +1,23 @@
 object BooleanSearch {
   /**
-   * Search for a term in an index.
+   * Search an index map for a term and return a list of results.
    * 
-   * @param index a map of inverted index (term -> List[docId])
-   * @param term a term to search for
-   * @return a list of docIds
+   * @param index A map of inverted index (term -> List[docId])
+   * @param term The term to search for
+   * @return A list of results, sorted in ascending order. If not found, an empty list is returned.
    */
   def search(index: Map[String, List[Int]], term: String): List[Int] = {
     index.getOrElse(term, List.empty).sorted
   }
 
   /**
-   * Search for
+   * Search an index map for two terms and return a list of the intersecting results.
    *
-   * @param index
-   * @param term1
-   * @param term2
-   * @return
+   * @param index A map of strings to a list of integers
+   * @param term1 The first term to search for
+   * @param term2 The second term to search for
+   * @return A list of the intersecting results from the two searches, sorted in ascending order. 
+   * If not found, an empty list is returned.
    */
   def searchAnd(index: Map[String, List[Int]], term1: String, term2: String): List[Int] = {
     val q1 = search(index, term1)
@@ -24,7 +25,16 @@ object BooleanSearch {
 
     q1.intersect(q2).sorted
   }
-
+  
+  /**
+    * Search an index map for two terms and return a list of the combined results.
+    *
+    * @param index A map of strings to a list of integers
+    * @param term1 The first term to search for
+    * @param term2 The second term to search for
+    * @return A list of the combined results from the two searches, sorted in ascending order. 
+    * If not found, an empty list is returned.
+    */
   def searchOr(index: Map[String, List[Int]], term1: String, term2: String): List[Int] = {
     val q1 = search(index, term1)
     val q2 = search(index, term2)
@@ -32,6 +42,17 @@ object BooleanSearch {
     (q1 ++ q2).distinct.sorted
   }
 
+  /**
+    * Search an index map for two terms and return a list of integers 
+    * which are in the list associated with term1 but 
+    * not in the list associated with term2.
+    *
+    * @param index A map of strings to a list of integers
+    * @param term1 The first term to search for
+    * @param term2 The second term to search for
+    * @return A list of the results from term1 but not from term2, sorted in ascending order. 
+    * If not found, an empty list is returned.
+    */
   def searchAndNot(index: Map[String, List[Int]], term1: String, term2: String): List[Int] = {
     val q1 = search(index, term1)
     val q2 = search(index, term2)
@@ -40,13 +61,16 @@ object BooleanSearch {
   }
 
   /**
-    * TODO: Add document
+    * Search an index map for two terms and return a list of integers 
+    * which are in the list associated with term1 or 
+    * not in the list associated with term2.
     *
-    * @param index
-    * @param term1
-    * @param term2
-    * @return
-    */
+    * @param index A map of strings to a list of integers
+    * @param term1 The first term to search for
+    * @param term2 The second term to search for
+    * @return A list of the results from term1 or not from term2, sorted in ascending order. 
+    * If not found, an empty list is returned.
+    */  
   def searchOrNot(index: Map[String, List[Int]], term1: String, term2: String): List[Int] = {
     val q1 = search(index, term1)
     val q2 = search(index, term2)
